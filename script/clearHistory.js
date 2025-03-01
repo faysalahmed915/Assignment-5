7fb39b6613f55548e70283895fe1e5b519d328cd
faysalahmed915 <faysalahmed915@gmail.com>
2025-03-01 18:24:58 +0600

    random bg is done and clear history btn done

---
+++ script/clearHistory.js
@@ -0,0 +1,5 @@
+document.getElementById("clear-history").addEventListener("click", function () {
+    // alert("Your history will be gone.")
+    document.getElementById("activity-container").innerHTML = "";
+}
+)
